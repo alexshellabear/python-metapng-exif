3ac2d9b168e5dbf590ccaffbc1ec1a18f4295f46
Alexander Shellabear <alexshellabear@gmail.com>
2020-04-04 10:08:08 +0800

    v0.5
    Trialing PIL Image object type

--- 02 Scripts/read_png_meta_data.py
+++ 02 Scripts/read_png_meta_data.py
@@ -1,13 +1,26 @@
 import dotenv # Get enironmental variables from .env file
-import subprocesses # Run exif tool
-import piexif
-from PIL import Image
+import os # Use dotenv to load environmental variables
+
+import piexif # Core module, read/write metadata
+from PIL import Image # Open images
+
+#import logging # Used to keep track of program progress
 
 class load_env_variables():
     def __init__(self):
         self.test_image_path = os.getenv("TEST_IMAGE_PATH")
 
+def return_all_exif(image_object):
+    if type(image_object) == "":
+        pass
+    else # TODO change to logging
+        raise TypeError(f"ERROR[return_all_exif]: Passed object is of type |{type(image_object)}| it should be of type ||")
+
 if __name__ == "__main__":
     print("starting...")
     settings = load_env_variables()
-    exif_data = exif
+    image_object = Image.open(settings.test_image_path)
+    exif_dictionary = piexif.load(image_object)
+    print(typ(image_object))
+    print(exif_dictionary)
+
